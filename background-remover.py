d800465a88c3eebf352e91be02a97a720cc12cac
manu12121999 <66686353+manu12121999@users.noreply.github.com>
2021-05-10 01:27:21 +0200

    Update background-remover.py

--- background-remover.py
+++ background-remover.py
@@ -16,8 +16,6 @@ from gi.repository import GLib
 from gi.repository import Gio
 
 import gettext
-_ = gettext.gettext
-
 
 baseLoc = os.path.dirname(os.path.realpath(__file__))+'/'
 sys.path.extend([baseLoc+'gimpenv/lib/python2.7', baseLoc+'gimpenv/lib/python2.7/site-packages', baseLoc+'gimpenv/lib/python2.7/site-packages/setuptools'])
@@ -28,7 +26,7 @@ sys.path.extend([baseLoc+'gimpenv/lib/python3.9', baseLoc+'gimpenv/lib/python3.9
 
 sys.path.extend([baseLoc+'gimpenv/Lib', baseLoc+'gimpenv/Lib/site-packages', baseLoc+'gimpenv/Lib/site-packages/setuptools'])
 
-# pip install numpy, torch, torchvision,PIL, opencv-python
+# pip install numpy, torch, torchvision,pillow, opencv-python
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -39,31 +37,8 @@ from Ldf.net import LDF
 import Ldf.test
 import Ldf.dataset
 
-
+_ = gettext.gettext
 def N_(message): return message
-
-def remove_background(procedure, run_mode, image, drawable, args, data):
-
-    image.undo_group_start()
-
-    file1 = Gio.file_new_for_path(os.path.join(tempfile.gettempdir(), "image_in.png"))
-    file2 = Gio.file_new_for_path(os.path.join(tempfile.gettempdir(), "image_out.png"))
- 
-    #create Input for LDF
-    Gimp.file_save(Gimp.RunMode.NONINTERACTIVE, image, [drawable], file1)
-
-    #Interference
-    Ldf.test.Test(Ldf.dataset, LDF, "./").save()
-
-    #output
-    result_layer = Gimp.file_load_layer(Gimp.RunMode.NONINTERACTIVE, image, file2)
-    mask = result_layer.create_mask(5)
-    drawable.add_mask(mask)
-
-    image.undo_group_end()
-    Gimp.displays_flush()
-
-    return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
 
 
 class BackgroundRemover(Gimp.PlugIn):
@@ -78,22 +53,44 @@ class BackgroundRemover(Gimp.PlugIn):
 
     def do_create_procedure(self, name):
         procedure = None
-        if name == 'python-fu-background-remover':
-            procedure = Gimp.ImageProcedure.new(self, name,
-                                                Gimp.PDBProcType.PLUGIN,
-                                                remove_background, None)
-            procedure.set_image_types("*")
-            procedure.set_documentation(
-                N_("Easy way to remove any background from an image"),
-                "test",  
-                name)
-            procedure.set_menu_label(N_("Background Remover"))
-            procedure.set_attribution("Manuel Vogel",
-                                      "Manuel Vogel",
-                                      "2021")
-            procedure.add_menu_path("<Image>/Filters/RemoveBackground")
+        
+        procedure = Gimp.ImageProcedure.new(self, name,
+                                            Gimp.PDBProcType.PLUGIN,
+                                            self.remove_background, None)
+        procedure.set_image_types("*")
+        procedure.set_documentation(
+            N_("Easy way to remove any background from an image"),
+            "test",  
+            name)
+        procedure.set_menu_label(N_("Background Remover"))
+        procedure.set_attribution("Manuel Vogel",
+                                  "Manuel Vogel",
+                                  "2021")
+        procedure.add_menu_path("<Image>/Filters/RemoveBackground")
 
         return procedure
+        
+    def remove_background(self, procedure, run_mode, image, n_drawables, drawables, args, data):
+        image.undo_group_start()
+
+        file1 = Gio.file_new_for_path(os.path.join(tempfile.gettempdir(), "image_in.png"))
+        file2 = Gio.file_new_for_path(os.path.join(tempfile.gettempdir(), "image_out.png"))
+
+        #create Input for LDF
+        Gimp.file_save(Gimp.RunMode.NONINTERACTIVE, image, [drawables[0]], file1)
+
+        #Interference
+        Ldf.test.Test(Ldf.dataset, LDF, "./").save()
+
+        #output
+        result_layer = Gimp.file_load_layer(Gimp.RunMode.NONINTERACTIVE, image, file2)
+        mask = result_layer.create_mask(5)
+        drawables[0].add_mask(mask)
+
+        image.undo_group_end()
+        Gimp.displays_flush()
+
+        return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
         
  
 Gimp.main(BackgroundRemover.__gtype__, sys.argv)

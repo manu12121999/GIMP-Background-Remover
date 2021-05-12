bf79531ae3a03a284c246fb2f3e7453c62292846
manu12121999 <66686353+manu12121999@users.noreply.github.com>
2021-05-12 19:18:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ source gimpenv/bin/activate
 python3 -m pip install numpy
 python3 -m pip install pillow
 python3 -m pip install torch
-python3 -m pip install torchvision
+#python3 -m pip install torchvision
 python3 -m pip install opencv-python
 deactivate
